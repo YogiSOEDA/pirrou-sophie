56d04faf25732a0f0b91155f6c2d2ecb1afb87c5
YogiSOEDA <yogiajus87@gmail.com>
2024-01-10 19:58:44 +0800

    add submitan 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,8 +17,8 @@ const paper11 = document.querySelector("#p11");
 const paper12 = document.querySelector("#p12");
 const paper13 = document.querySelector("#p13");
 const paper14 = document.querySelector("#p14");
-const paper15 = document.querySelector("#p15");
-const paper16 = document.querySelector("#p16");
+// const paper15 = document.querySelector("#p15");
+// const paper16 = document.querySelector("#p16");
 
 const audio = document.getElementById('audio');
 const btnMute = document.getElementById('mute-btn');
@@ -34,7 +34,7 @@ btnMute.addEventListener("click", changeIcon);
 
 // Business Logic
 let currentState = 1;
-let numOfPapers = 16;
+let numOfPapers = 14;
 let maxState = numOfPapers + 1;
 
 function openBook() {
@@ -118,18 +118,19 @@ function goNext() {
         paper13.style.zIndex = 13;
         break;
       case 14:
+        closeBook(false);
         paper14.classList.add("flipped");
         paper14.style.zIndex = 14;
         break;
-      case 15:
-        paper15.classList.add("flipped");
-        paper15.style.zIndex = 15;
-        break;
-      case 16:
-        closeBook(false);
-        paper16.classList.add("flipped");
-        paper16.style.zIndex = 16;
-        break;
+      // case 15:
+      //   paper15.classList.add("flipped");
+      //   paper15.style.zIndex = 15;
+      //   break;
+      // case 16:
+      //   closeBook(false);
+      //   paper16.classList.add("flipped");
+      //   paper16.style.zIndex = 16;
+      //   break;
       default:
         throw new Error("unkown state");
     }
@@ -145,72 +146,74 @@ function goPrevious() {
         closeBook(true);
         // removeIframe();
         paper1.classList.remove("flipped");
-        paper1.style.zIndex = 16;
+        paper1.style.zIndex = 14;
         break;
       case 3:
         // removeIframe();
         paper2.classList.remove("flipped");
-        paper2.style.zIndex = 15;
+        paper2.style.zIndex = 13;
         break;
       case 4:
         paper3.classList.remove("flipped");
-        paper3.style.zIndex = 14;
+        paper3.style.zIndex = 12;
         break;
       case 5:
         paper4.classList.remove("flipped");
-        paper4.style.zIndex = 13;
+        paper4.style.zIndex = 11;
         break;
       case 6:
         paper5.classList.remove("flipped");
-        paper5.style.zIndex = 12;
+        paper5.style.zIndex = 10;
         break;
       case 7:
         paper6.classList.remove("flipped");
-        paper6.style.zIndex = 11;
+        paper6.style.zIndex = 9;
         break;
       case 8:
         paper7.classList.remove("flipped");
-        paper7.style.zIndex = 10;
+        paper7.style.zIndex = 8;
         break;
       case 9:
         paper8.classList.remove("flipped");
-        paper8.style.zIndex = 9;
+        paper8.style.zIndex = 7;
         break;
       case 10:
         paper9.classList.remove("flipped");
-        paper9.style.zIndex = 8;
+        paper9.style.zIndex = 6;
         break;
       case 11:
         paper10.classList.remove("flipped");
-        paper10.style.zIndex = 7;
+        paper10.style.zIndex = 5;
         break;
       case 12:
         paper11.classList.remove("flipped");
-        paper11.style.zIndex = 6;
+        paper11.style.zIndex = 4;
         break;
       case 13:
         paper12.classList.remove("flipped");
-        paper12.style.zIndex = 5;
+        paper12.style.zIndex = 3;
         break;
       case 14:
         paper13.classList.remove("flipped");
-        paper13.style.zIndex = 4;
+        paper13.style.zIndex = 2;
         break;
       case 15:
-        paper14.classList.remove("flipped");
-        paper14.style.zIndex = 3;
-        break;
-      case 16:
-        // removeIframe();
-        paper15.classList.remove("flipped");
-        paper15.style.zIndex = 2;
-        break;
-      case 17:
         openBook();
-        paper16.classList.remove("flipped");
-        paper16.style.zIndex = 1;
+        paper14.classList.remove("flipped");
+        paper14.style.zIndex = 1;
         removeIframe();
         break;
+      // case 16:
+      //   // removeIframe();
+      //   paper15.classList.remove("flipped");
+      //   paper15.style.zIndex = 2;
+      //   break;
+      // case 17:
+      //   openBook();
+      //   paper16.classList.remove("flipped");
+      //   paper16.style.zIndex = 1;
+      //   removeIframe();
+      //   break;
     }
 
     currentState--;
